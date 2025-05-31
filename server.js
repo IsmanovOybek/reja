7ccbfd1b98c790bacab59058a17f3bc182f4b9e4
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-05-31 15:48:46 +0900

    feat  ejs freamwork

--- server.js
+++ server.js
@@ -14,12 +14,16 @@ app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 //4 routing kod
-app.get("/hello", function (req, res) {
-    res.send(`<h1 style="background:red">Hello world </h1>`);
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" })
+
 })
-app.get("/gift", function (req, res) {
-    res.send(`Siz sovgala bolimidasz`);
+
+app.get("/", function (req, res) {
+    res.render(`harid`);
 })
+
 const server = http.createServer(app)
 let PORT = 3000;
 server.listen(PORT, function () {
